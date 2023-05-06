6c5cac1d264b8e49615c81630cc23b18faffebf7
Dr_pep4 <ozmeen@naver.com>
2023-05-06 14:54:53 +0900

    feat:메인페이지,마이페이지 기능추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
 
+
+
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'your_secret_key_here'
 
     # 블루프린트
-    from .views import main_views, catalog, for_link, login, mypage, cart, product, orders
+    from .views import main_views, login, mypage, cart, product, orders, main
 
     app.register_blueprint(main_views.bp)
-    app.register_blueprint(for_link.bp)
-    app.register_blueprint(catalog.bp)
+    app.register_blueprint(main.bp)
+
     app.register_blueprint(login.bp)
     app.register_blueprint(mypage.bp)
     app.register_blueprint(cart.bp)
@@ -18,5 +20,9 @@ def create_app():
 
     app.add_url_rule('/login', view_func=login, methods=['GET', 'POST'])
     
+
+   
+    app.secret_key = '1234'
+
 
     return app
--- pybo/views/login.py
+++ pybo/views/login.py
@@ -5,46 +5,50 @@ from flask import Blueprint, render_template, request, session, Flask, url_for, 
 from my_settings import PW
 
 
-    
+
+bp = Blueprint('login', __name__, url_prefix='/user' )
+
+db = pymysql.connect(host='localhost',            # database 접근
+                    user='root',
+                    password=PW,
+                    db='shop',
+                    charset='utf8mb4')
+
+
+cursor = db.cursor()                               # database 테이블에 접근  
 
 
 
+@bp.route('/sign_in')
+def login_page():
+    return render_template('/signin.html')
 
-bp = Blueprint('login', __name__, url_prefix='/users' )
-
-db = pymysql.connect(host='127.0.0.1', user='root', password=PW, db='shop', charset='utf8')
-
-cursor = db.cursor()
-
-@bp.route('/2')
-def subpage3():
-    return render_template('text.html')
-
-        
-@bp.route('/signin')
-def home():
-    return render_template('signin.html')
-
-@bp.route('/signin', methods=['GET', 'POST'])
+@bp.route('/sign_in', methods=['GET', 'POST'])
 def login():
-   
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         cursor = db.cursor()
-        cursor.execute('SELECT email, password FROM users WHERE email = %s AND password = %s;', (email, password))
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
         user = cursor.fetchone()
         if user is not None:
-            session['email'] = user[0]
-            session['password'] = user[1]
-            return render_template('user.html')
+            session['email'] = user[2]
+            session['password'] = user[3]
+            session['name']=user[1]
+            return redirect(url_for('main.main'))
         else:
-            return render_template('signin.html', error="이메일 또는 비밀번호가 올바르지 않습니다.")
+            return "이메일 또는 비밀번호가 올바르지 않습니다."
     else:
-        return redirect ('signin.html')
+        return render_template ('signin.html')
+    
+@bp.route('/sign_up')
+def sign_up_page():
+    return render_template('sign_up-2.html')
 
-@bp.route('/logout')
+@bp.route('/log_out')
 def logout():
     session.pop('email')
     session.pop('password')
-    return redirect(url_for('home'))
+    session.pop('name')
+    session.clear()
+    return redirect(url_for('main.main'))
