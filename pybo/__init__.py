537ce20f20e4e018bdfb493981015302c5c4e158
YetWhom <jason948742@gmail.com>
2023-05-08 16:13:13 +0900

    Feat:회원가입 기능 구현 완료

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,21 +4,13 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-<<<<<<< HEAD
-    app.secret_key = 'abab'
-    # 블루프린트
-    from .views import login, cart, product, orders, signup
-
-    
-=======
     app.secret_key = 'your_secret_key_here'
 
     # 블루프린트
-    from .views import main_views, login, mypage, cart, product, orders, main
+    from .views import main_views, login, mypage, cart, product, orders, main, signup
 
     app.register_blueprint(main_views.bp)
     app.register_blueprint(main.bp)
->>>>>>> 88058fefc1f5c08134a65761086b5c3d15c43a53
     app.register_blueprint(login.bp)
     app.register_blueprint(signup.bp)
     app.register_blueprint(cart.bp)
