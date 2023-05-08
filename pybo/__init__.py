d5a458bcfeb2ed60a6bb8ed125ccd0fe80d6ebdd
kiietls23 <kiietls23@Korea.ac.kr>
2023-05-08 16:34:25 +0900

    Feat: 장바구니 및 결제 페이지 html/css 통일

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,10 +4,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'your_secret_key_here'
+    app.secret_key = '1234'
 
     # 블루프린트
-    from .views import main_views, login, mypage, cart, product, orders, main
+    from .views import main_views, login, mypage, cart, product, orders, main, signup
 
     app.register_blueprint(main_views.bp)
     app.register_blueprint(main.bp)
@@ -16,12 +16,9 @@ def create_app():
     app.register_blueprint(cart.bp)
     app.register_blueprint(product.bp)
     app.register_blueprint(orders.bp)
+    app.register_blueprint(signup.bp)
 
     app.add_url_rule('/login', view_func=login, methods=['GET', 'POST'])
-    
-
-   
-    app.secret_key = '1234'
 
 
     return app
